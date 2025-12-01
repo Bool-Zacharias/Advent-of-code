3da1a9bdb30be06fef5341ce1d70d6de4e646e7f
Bool-Zacharias <h23zacan@du.se>
2025-12-01 20:17:39 +0100

    Password day 1

--- Day 1.py
+++ Day 1.py
@@ -1,17 +1,32 @@
-file = open("input.txt","r") 
+file = open("input.txt", "r")
 
-content = file.read()
-
+left = 0
+right = 0
 goal = 0
+amount = 0
 start = 50
+total_value = 50
 
 for line in file:
     if line[0] == "L":
-        left +=1
-    elif line[0] =="R":
-        right +=1
+        left += 1
+    elif line[0] == "R":
+        right += 1
 
-print("L:", left)
-print("L:", right)
+    direction = line[0]
+    number = line[1:]
+    value = int(number)
+
+    if direction == 'R':
+        total_value += value
+    elif direction == 'L':
+        total_value -= value
+    
+    # Modulus
+    total_value = total_value % 100
+
+    if total_value == goal:
+        amount += 1
+print(amount)
 
 file.close()
